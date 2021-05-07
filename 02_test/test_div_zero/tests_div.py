cabb07be66dcc608fab569fddb0da84daa9c8561
Jeremias Enriquez <capuche777@gmail.com>
2021-05-06 21:47:45 -0600

    Organized tests
    Lack of description, is a must

---
+++ 02_test/test_div_zero/tests_div.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestDiv(unittest.TestCase):
+    def test_div0(self):
+        pass
+
+    def test_div1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 02_test/tests_sum.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestSum(unittest.TestCase):
+    def test_sum0(self):
+        pass
+
+    def test_sum1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
